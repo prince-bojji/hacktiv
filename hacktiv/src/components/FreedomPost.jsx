218d9e40b941c240b41725553c34b4ce01a1e34d
Yuzyx17 <braitboat@gmail.com>
2023-09-05 13:09:49 +0800

    Freedompost finalization, freedomboard init

--- hacktiv/src/components/FreedomPost.jsx
+++ hacktiv/src/components/FreedomPost.jsx
@@ -1,50 +1,55 @@
 import React, { useRef, useState } from "react";
 
 function FreedomPost(props) {
+	// Remove this props object for dynamic content
+	props = props.props
 	const [isEditing, setIsEditing] = useState(false);
 	const [isDeleting, setIsDeleting] = useState(false);
 	const [isAuthor, setIsAuthor] = useState(true);
 
-  // When the delete button is clicked
+	const [postTitle, setTitle] = useState(props.title)
+	const [postContent, setContent] = useState(props.content)
+	const [properties, setProps] = useState(props)
+
+	// When the delete button is clicked
 	const onDeleting = () => {
 		setIsDeleting(true);
 	};
-  // When edit button is clicked
+	// When edit button is clicked
 	const onEdit = () => {
 		setIsEditing(true);
 	};
-  // When save on edit is clicked
-  const onSave = () => {
-
-  }
-  // When confirm delete button is clicked
-  const onDelete = () => {
-
-  }
-  // When edit or delete is cancelled
+	// When save on edit is clicked
+	const onSave = () => {
+		setProps({...props, title: postTitle, content: postContent})
+		setIsEditing(false)
+	};
+	// When confirm delete button is clicked
+	const onDelete = () => {};
+	// When edit or delete is cancelled
 	const onCancel = () => {
 		setIsDeleting(false);
 		setIsEditing(false);
 	};
 
-	// Remove this props object for dynamic content
-	const date = new Date();
-	props = {
-		title: "Lorem Ipsum",
-		content:
-			"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Euismod in pellentesque massa placerat duis ultricies lacus sed turpis. Lorem ipsum dolor sit amet consectetur adipiscing elit. Scelerisque fermentum dui faucibus in ornare quam viverra orci. Est ullamcorper eget nulla facilisi etiam dignissim diam quis enim. Id velit ut tortor pretium viverra suspendisse potenti nullam. Egestas erat imperdiet sed euismod nisi porta lorem mollis. Phasellus faucibus scelerisque eleifend donec pretium vulputate sapien nec ",
-		author: "Juan Dela Cruz",
-		date_posted:
-			date.getMonth() + "/" + date.getDay() + "/" + date.getFullYear(),
-		time_posted:
-			(date.getHours() % 12) +
-			":" +
-			date.getMinutes() +
-			(date.getHours % 12 == 0 ? "am" : "pm"),
-	};
+	const titleChange = (e) => {
+		setTitle(e.target.value)
+	}
+
+	const contentChange = (e) => {
+		setContent(e.target.value)
+	}
+
 	return (
 		<>
-			<div className={(isEditing ? "outline outline-offset-2 outline-secondary-a " : "outline-none ") + "bg-primary rounded p-8 xs:text-xs md:text-lg xl:text-2xl min-[320px]:w-[92%] md:w-[56%] xl:w-[32%] h-max shadow-[0_0_6px_2px_rgba(0,0,0,0.25)] ease-in duration-100"}>
+			<div
+				className={
+					(isEditing
+						? "outline outline-offset-2 outline-secondary-a "
+						: "outline-none ") +
+					"bg-primary rounded p-8 xs:text-xs md:text-lg xl:text-2xl min-[320px]:w-[92%] md:w-[56%] xl:w-[32%] h-max shadow-[0_0_6px_2px_rgba(0,0,0,0.25)] ease-in duration-100"
+				}
+			>
 				<div className="grid grid-cols-3 items-center mb-5">
 					<div className="flex items-center col-span-2">
 						{/* replace this span for actual image */}
@@ -53,6 +58,97 @@ function FreedomPost(props) {
 					</div>
 					{/* If author is the same is the user, show these buttons */}
 					{isAuthor ? (
+						<>
+							<div className="flex justify-self-end gap-4">
+								<button
+									className={
+										(isEditing
+											? "bg-accent w-max p-4 rounded-md font-bold "
+											: isDeleting
+											? "bg-warning w-max p-4 rounded-md font-bold "
+											: "bg-accent min-[320px]:w-10 lg:w-12 rounded-full aspect-square ") +
+										"ease-in duration-75"
+									}
+									onClick={isEditing ? onSave : isDeleting ? onDelete : onEdit}
+								>
+									{isEditing ? (
+										"Save"
+									) : isDeleting ? (
+										"Confirm"
+									) : (
+										<i className="fa-solid fa-pen-to-square"></i>
+									)}
+								
+								</button>
+								<button
+									className={
+										(isDeleting ? "bg-accent" : "bg-warning") +
+										" min-[320px]:w-10 lg:w-12 rounded-full aspect-square ease-in duration-75"
+									}
+									onClick={
+										isEditing ? onCancel : isDeleting ? onCancel : onDeleting
+									}
+								>
+									{isEditing ? (
+										<i
+											className="fa-solid fa-xmark min-[320px]:fa-xs"
+											style={{ color: "#ffffff" }}
+										></i>
+									) : isDeleting ? (
+										<i
+											className="fa-solid fa-xmark min-[320px]:fa-xs"
+											style={{ color: "#ffffff" }}
+										></i>
+									) : (
+										<i className="fa-solid fa-trash min-[320px]:fa-xs"></i>
+									)}
+								</button>
+							</div>
+						</>
+					) : (
+						<></>
+					)}
+					
+				</div>
+				<div className="flex flex-col mb-4">
+					{isEditing ? (
+						<>
+							<input
+								name="title"
+								type="text"
+								className="border-tertiary-b border border-solid shadow-inner rounded-lg font-black min-[320px]:text-3xl lg:text-4xl my-4 h-max p-2"
+								value={postTitle}
+								onChange={titleChange}
+								maxLength="25"
+							/>
+							<textarea
+								name="content"
+								className="border-tertiary-b border border-solid shadow-[inset_0_0_2px_1px_rgba(0,0,0,0.25)] rounded-lg text-justify h-[320px] resize-none p-2"
+								value={postContent}
+								onChange={contentChange}
+								maxLength="1000"
+							/>
+						</>
+					) : (
+						<>
+							<span className="font-black min-[320px]:text-3xl lg:text-4xl my-4">
+								{properties.title}
+							</span>
+							<span className="text-justify break-words ">{properties.content}</span>
+						</>
+					)}
+				</div>
+				<div className="float-right flex flex-row w-max gap-8 font-extralight text-sm mt-2">
+					<span className="text-tertiary-a">{props.date_posted}</span>
+					<span className="text-tertiary-a">{props.time_posted}</span>
+				</div>
+			</div>
+		</>
+	);
+}
+
+export default FreedomPost;
+{/* {isAuthor ? (
 						<div className="flex justify-self-end gap-4">
 							{!isEditing && !isDeleting ? (
 								<>
@@ -71,20 +167,28 @@ function FreedomPost(props) {
 								</>
 							) : isEditing ? (
 								<>
+									<button className="bg-accent w-max p-2 rounded-md font-bold">
+										Save
+									</button>
 									<button
-                    className="bg-accent w-max p-2 rounded-md font-bold">Save</button>
-									<button 
-                    className="bg-warning min-[320px]:w-8 lg:w-12 rounded-full aspect-square"
-                    onClick={onCancel}>
-										<i className="fa-solid fa-xmark min-[320px]:fa-xs" style={{color: '#ffffff'}}></i>
+										className="bg-warning min-[320px]:w-8 lg:w-12 rounded-full aspect-square"
+										onClick={onCancel}
+									>
+										<i
+											className="fa-solid fa-xmark min-[320px]:fa-xs"
+											style={{ color: "#ffffff" }}
+										></i>
 									</button>
 								</>
 							) : (
 								<>
-									<button className="bg-warning w-max p-2 rounded-md font-bold">Confirm</button>
-									<button 
-                    className="bg-accent min-[320px]:w-8 lg:w-12 rounded-full aspect-square"
-                    onClick={onCancel}>
+									<button className="bg-warning w-max p-2 rounded-md font-bold">
+										Confirm
+									</button>
+									<button
+										className="bg-accent min-[320px]:w-8 lg:w-12 rounded-full aspect-square"
+										onClick={onCancel}
+									>
 										<i className="fa-solid fa-xmark min-[320px]:fa-xs"></i>
 									</button>
 								</>
@@ -92,40 +196,4 @@ function FreedomPost(props) {
 						</div>
 					) : (
 						<></>
-					)}
-				</div>
-				<div className="flex flex-col mb-4">
-					{isEditing ? (
-						<>
-							<input
-								name="title"
-                readOnly={isEditing}
-								className="border-tertiary-b border border-solid shadow-inner rounded-lg font-black min-[320px]:text-3xl lg:text-4xl my-4 h-max p-2"
-								defaultValue={props.title}
-							/>
-							<textarea
-								name="content"
-                readOnly={isEditing}
-								className="border-tertiary-b border border-solid shadow-[inset_0_0_2px_1px_rgba(0,0,0,0.25)] rounded-lg text-justify h-[420px] resize-none p-2"
-								defaultValue={props.content}
-							/>
-						</>
-					) : (
-						<>
-							<span className="font-black min-[320px]:text-3xl lg:text-4xl my-4">
-								{props.title}
-							</span>
-							<span className="text-justify">{props.content}</span>
-						</>
-					)}
-				</div>
-				<div className="float-right flex flex-row w-max gap-8 font-extralight text-sm mt-2">
-					<span className="text-tertiary-a">{props.date_posted}</span>
-					<span className="text-tertiary-a">{props.time_posted}</span>
-				</div>
-			</div>
-		</>
-	);
-}
-
-export default FreedomPost;
+					)} */}
--- hacktiv/src/pages/FreedomBoard.jsx
+++ hacktiv/src/pages/FreedomBoard.jsx
@@ -2,10 +2,50 @@ import React from 'react'
 import FreedomPost from '../components/FreedomPost'
 
 function FreedomBoard() {
+  const date = new Date();
+  const testData = [{
+		title: "Lorem Ipsum",
+		content:
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Euismod in pellentesque massa placerat duis ultricies lacus sed turpis. Lorem ipsum dolor sit amet consectetur adipiscing elit. Scelerisque fermentum dui faucibus in ornare quam viverra orci. Est ullamcorper eget nulla facilisi etiam dignissim diam quis enim. Id velit ut tortor pretium viverra suspendisse potenti nullam. Egestas erat imperdiet sed euismod nisi porta lorem mollis. Phasellus faucibus scelerisque eleifend donec pretium vulputate sapien nec ",
+		author: "Juan Dela Cruz",
+		date_posted:
+			date.getMonth() + "/" + date.getDay() + "/" + date.getFullYear(),
+		time_posted:
+			(date.getHours() % 12) +
+			":" +
+			date.getMinutes() +
+			(date.getHours % 12 == 0 ? "am" : "pm"),
+	},{
+		title: "Timmy Dayo",
+		content:
+			"Libero enim sed faucibus turpis in eu mi bibendum. Ornare arcu odio ut sem nulla pharetra diam sit amet. Non arcu risus quis varius quam quisque id diam. Egestas congue quisque egestas diam in arcu. Ipsum dolor sit amet consectetur adipiscing elit. Nam aliquam sem et tortor consequat id porta. Donec enim diam vulputate ut pharetra sit. Commodo sed egestas egestas fringilla. Tempor orci dapibus ultrices in iaculis. Consectetur adipiscing elit ut aliquam purus sit amet luctus. Cras tincidunt lobortis feugiat vivamus at augue eget arcu. Nisi porta lorem mollis aliquam ut porttitor leo. Praesent elementum facilisis leo vel fringilla est ullamcorper eget. Ipsum consequat nisl vel pretium lectus quam id. Et odio pellentesque diam volutpat commodo sed egestas egestas. ",
+		author: "Juan Dela Cruz",
+		date_posted:
+			date.getMonth() + "/" + date.getDay() + "/" + date.getFullYear(),
+		time_posted:
+			(date.getHours() % 12) +
+			":" +
+			date.getMinutes() +
+			(date.getHours % 12 == 0 ? "am" : "pm"),
+	},{
+		title: "Mumbai World",
+		content:
+			"Convallis tellus id interdum velit laoreet id donec ultrices tincidunt. Tellus integer feugiat scelerisque varius morbi. Tristique senectus et netus et malesuada. Donec massa sapien faucibus et molestie ac feugiat sed lectus. In metus vulputate eu scelerisque felis imperdiet proin fermentum. Non tellus orci ac auctor augue mauris augue neque gravida. Condimentum id venenatis a condimentum vitae. Pretium aenean pharetra magna ac placerat vestibulum lectus. Lacus laoreet non curabitur gravida arcu ac. Ac tortor vitae purus faucibus ornare suspendisse. Lacus suspendisse faucibus interdum posuere lorem ipsum. Facilisis sed odio morbi quis. Eget nunc scelerisque viverra mauris in aliquam. Tincidunt arcu non sodales neque sodales ut. Eu lobortis elementum nibh tellus. Sagittis eu volutpat odio facilisis mauris sit amet.",
+		author: "Juan Dela Cruz",
+		date_posted:
+			date.getMonth() + "/" + date.getDay() + "/" + date.getFullYear(),
+		time_posted:
+			(date.getHours() % 12) +
+			":" +
+			date.getMinutes() +
+			(date.getHours % 12 == 0 ? "am" : "pm"),
+	}]
   return (
     <>
-      <div className="font-inter flex justify-center items-center min-h-[100vh] h-max">
-        <FreedomPost />
+      <div className="font-inter flex justify-center items-start h-max gap-4">
+        {testData.map((val) => 
+          <FreedomPost props={val} key={val.date_posted+val.time_posted+testData.indexOf(val)}/>
+        )}
       </div>
     </>
   )
