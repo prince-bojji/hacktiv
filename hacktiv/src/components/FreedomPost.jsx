7bc91ca005401748719277f6eabc961a3d21983f
Yuzyx17 <braitboat@gmail.com>
2023-09-06 21:32:32 +0800

    complete responsiveness for freedomboard

--- hacktiv/src/components/FreedomPost.jsx
+++ hacktiv/src/components/FreedomPost.jsx
@@ -2,7 +2,7 @@ import React, { useRef, useState } from "react";
 
 function FreedomPost(props) {
 	// Remove this props object for dynamic content
-	props = props.props
+	props = props.post
 	const [isEditing, setIsEditing] = useState(false);
 	const [isDeleting, setIsDeleting] = useState(false);
 	const [isAuthor, setIsAuthor] = useState(true);
@@ -11,27 +11,19 @@ function FreedomPost(props) {
 	const [postContent, setContent] = useState(props.content)
 	const [properties, setProps] = useState(props)
 
-	// When the delete button is clicked
-	const onDeleting = () => {
-		setIsDeleting(true);
-	};
-	// When edit button is clicked
-	const onEdit = () => {
-		setIsEditing(true);
-	};
-	// When save on edit is clicked
+	// When save edit button is clicked
 	const onSave = () => {
 		setProps({...props, title: postTitle, content: postContent})
+		// properties now contain all the changes, may be used for CRUD, render is only client side not server side, unless Freedomboard will be
+		// re-rendered for each save
 		setIsEditing(false)
 	};
 	// When confirm delete button is clicked
-	const onDelete = () => {};
-	// When edit or delete is cancelled
-	const onCancel = () => {
-		setIsDeleting(false);
-		setIsEditing(false);
+	const onConfirmDelete = () => {
+		
 	};
 
+	// These functions are for handling state changes
 	const titleChange = (e) => {
 		setTitle(e.target.value)
 	}
@@ -40,50 +32,63 @@ function FreedomPost(props) {
 		setContent(e.target.value)
 	}
 
+	const onDeleting = () => {
+		setIsDeleting(true);
+	};
+
+	const onEdit = () => {
+		setIsEditing(true);
+	};
+
+	const onCancel = () => {
+		setIsDeleting(false);
+		setIsEditing(false);
+	};
+
 	return (
-		<>
 			<div
 				className={
 					(isEditing
 						? "outline outline-offset-2 outline-secondary-a "
 						: "outline-none ") +
-					"bg-primary rounded p-8 xs:text-xs md:text-lg xl:text-2xl min-[320px]:w-[92%] md:w-[56%] xl:w-[32%] h-max shadow-[0_0_6px_2px_rgba(0,0,0,0.25)] ease-in duration-100"
+						// min-[320px]:w-[92%] md:w-[56%] xl:w-[32%]
+					"bg-primary flex flex-col flex-1 rounded py-3 px-6 xs:text-xs md:text-lg xl:text-2xl m-0 h-max lg:shadow-[0_0_6px_2px_rgba(0,0,0,0.25)] shadow-[0_0_2px_1px_rgba(0,0,0,0.25)] ease-in duration-100"
 				}
 			>
 				<div className="grid grid-cols-3 items-center mb-5">
 					<div className="flex items-center col-span-2">
 						{/* replace this span for actual image */}
-						<span className="bg-tertiary-b rounded-full min-[320px]:w-16 md:w-14 lg:w-20 aspect-square mr-2" />
+						<span className="bg-tertiary-c rounded-full min-[320px]:w-14 md:w-12 lg:w-16 aspect-square mr-2" />
 						<span className="font-semibold">{props.author}</span>
 					</div>
-					{/* If author is the same is the user, show these buttons */}
+					{/* If author is the same as the user, show these buttons */}
 					{isAuthor ? (
 						<>
-							<div className="flex justify-self-end gap-4">
+							<div className="flex justify-self-end gap-4 items-center">
 								<button
 									className={
 										(isEditing
-											? "bg-accent w-max p-4 rounded-md font-bold "
+											? "bg-accent w-max px-2 py-1 rounded-md font-bold "
 											: isDeleting
-											? "bg-warning w-max p-4 rounded-md font-bold "
-											: "bg-accent min-[320px]:w-10 lg:w-12 rounded-full aspect-square ") +
-										"ease-in duration-75"
+											? "bg-warning w-max px-2 py-1 rounded-md font-bold "
+											: "bg-accent min-[320px]:h-8 h-10 rounded-full aspect-square ") +
+										"ease-in duration-75 flex items-center justify-center"
 									}
-									onClick={isEditing ? onSave : isDeleting ? onDelete : onEdit}
+									onClick={isEditing ? onSave : isDeleting ? onConfirmDelete : onEdit}
 								>
 									{isEditing ? (
 										"Save"
 									) : isDeleting ? (
 										"Confirm"
 									) : (
-										<i className="fa-solid fa-pen-to-square"></i>
+										<i className="fa-solid fa-pen-to-square min-[320px]:fa-2xs fa-xs "></i>
 									)}
 								
 								</button>
 								<button
 									className={
 										(isDeleting ? "bg-accent" : "bg-warning") +
-										" min-[320px]:w-10 lg:w-12 rounded-full aspect-square ease-in duration-75"
+										" min-[320px]:h-8 h-10 rounded-full aspect-square ease-in duration-75 flex items-center justify-center"
 									}
 									onClick={
 										isEditing ? onCancel : isDeleting ? onCancel : onDeleting
@@ -91,16 +96,16 @@ function FreedomPost(props) {
 								>
 									{isEditing ? (
 										<i
-											className="fa-solid fa-xmark min-[320px]:fa-xs"
+											className="fa-solid fa-xmark min-[320px]:fa-2xs fa-xs "
 											style={{ color: "#ffffff" }}
 										></i>
 									) : isDeleting ? (
 										<i
-											className="fa-solid fa-xmark min-[320px]:fa-xs"
+											className="fa-solid fa-xmark min-[320px]:fa-2xs fa-xs"
 											style={{ color: "#ffffff" }}
 										></i>
 									) : (
-										<i className="fa-solid fa-trash min-[320px]:fa-xs"></i>
+										<i className="fa-solid fa-trash min-[320px]:fa-2xs fa-xs "></i>
 									)}
 								</button>
 							</div>
@@ -123,7 +128,7 @@ function FreedomPost(props) {
 							/>
 							<textarea
 								name="content"
-								className="border-tertiary-b border border-solid shadow-[inset_0_0_2px_1px_rgba(0,0,0,0.25)] rounded-lg text-justify h-[320px] resize-none p-2"
+								className="border-tertiary-b border border-solid shadow-[inset_0_0_2px_1px_rgba(0,0,0,0.25)] rounded-lg font-regular text-justify h-[320px] resize-none p-2"
 								value={postContent}
 								onChange={contentChange}
 								maxLength="1000"
@@ -134,16 +139,15 @@ function FreedomPost(props) {
 							<span className="font-black min-[320px]:text-3xl lg:text-4xl my-4">
 								{properties.title}
 							</span>
-							<span className="text-justify break-words ">{properties.content}</span>
+							<span className="font-normal text-justify break-words ">{properties.content}</span>
 						</>
 					)}
 				</div>
-				<div className="float-right flex flex-row w-max gap-8 font-extralight text-sm mt-2">
-					<span className="text-tertiary-a">{props.date_posted}</span>
-					<span className="text-tertiary-a">{props.time_posted}</span>
+				<div className="self-end flex flex-row w-max gap-8 font-light text-md mt-3">
+					<span className="text-secondary">{props.date_posted}</span>
+					<span className="text-secondary">{props.time_posted}</span>
 				</div>
 			</div>
-		</>
 	);
 }
 
