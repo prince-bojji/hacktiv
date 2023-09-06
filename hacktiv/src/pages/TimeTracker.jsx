c033cb4d17475ffddea49ef9a104b623d3b2a960
CedrickTolentino <johncedrick0408@gmail.com>
2023-09-06 22:37:19 +0800

    timer edit

--- hacktiv/src/pages/TimeTracker.jsx
+++ hacktiv/src/pages/TimeTracker.jsx
@@ -4,12 +4,40 @@ import 'react-datepicker/dist/react-datepicker.css';
 
 function TimeTracker(props) {
   const [selectedDate, setSelectedDate] = useState(null);
+  const [timerRunning, setTimerRunning] = useState(false);
+  const [timerValue, setTimerValue] = useState(0);
 
   const handleDateChange = (date) => {
     setSelectedDate(date);
   };
+
+  const startTimer = () => {
+    setTimerRunning(true);
+    let startTime = Date.now();
+
+    const timerInterval = setInterval(() => {
+      const currentTime = Date.now();
+      const elapsedTime = (currentTime - startTime) / 1000;
+      setTimerValue(elapsedTime);
+    }, 1000);
+
+    return () => {
+      clearInterval(timerInterval);
+    };
+  };
+
+  const stopTimer = () => {
+    setTimerRunning(false);
+  };
+
   return (
     <div>
+      <div>
+        <button onClick={timerRunning ? stopTimer : startTimer}>
+          {timerRunning ? 'Stop Timer' : 'Start Timer'}
+        </button>
+        <span>Timer: {timerValue} seconds</span>
+      </div>
       <input
         type="text"
         placeholder="Search..."
