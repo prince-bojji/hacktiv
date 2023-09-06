45f8ad24402e51506a9be66093d9e971f1f9b8ce
CedrickTolentino <johncedrick0408@gmail.com>
2023-09-06 23:04:27 +0800

    timer edit

--- hacktiv/src/pages/TimeTracker.jsx
+++ hacktiv/src/pages/TimeTracker.jsx
@@ -7,27 +7,32 @@ function TimeTracker(props) {
   const [timerRunning, setTimerRunning] = useState(false);
   const [timerValue, setTimerValue] = useState(0);
 
+  const [startTime, setStartTime] = useState(null);
+
   const handleDateChange = (date) => {
     setSelectedDate(date);
   };
 
   const startTimer = () => {
-    setTimerRunning(true);
-    let startTime = Date.now();
+    if (!timerRunning) {
+      setStartTime(Date.now());
+      setTimerRunning(true);
 
-    const timerInterval = setInterval(() => {
-      const currentTime = Date.now();
-      const elapsedTime = (currentTime - startTime) / 1000;
-      setTimerValue(elapsedTime);
-    }, 1000);
+      const timerInterval = setInterval(() => {
+        const currentTime = Date.now();
+        const elapsedTime = (currentTime - startTime) / 1000;
+        setTimerValue(elapsedTime);
+      }, 1000);
 
-    return () => {
-      clearInterval(timerInterval);
-    };
-  };
-
-  const stopTimer = () => {
-    setTimerRunning(false);
+      return () => {
+        clearInterval(timerInterval);
+      };
+    } else {
+      // Stop the timer if it's already running
+      setTimerRunning(false);
+      setTimerValue(0);
+      setStartTime(null);
+    }
   };
 
   return (
@@ -42,19 +47,23 @@ function TimeTracker(props) {
           selected={selectedDate}
           onChange={handleDateChange}
           placeholderText="Select a date"
-          style={{ marginLeft: '10px' }} // Add margin for spacing
+          style={{ marginLeft: '10px' }}
         />
         <button
-          onClick={timerRunning ? stopTimer : startTimer}
+          onClick={startTimer}
           style={{
             backgroundColor: 'yellow',
             color: 'black',
-            marginLeft: '10px', // Add margin for spacing
+            marginLeft: '10px',
           }}
         >
           {timerRunning ? 'Stop Timer' : 'Start Timer'}
         </button>
-        <span> {timerValue} seconds</span>
+        <span>
+          {Math.floor(timerValue / 3600).toString().padStart(2, '0')}:
+          {Math.floor((timerValue % 3600) / 60).toString().padStart(2, '0')}:
+          {(timerValue % 60).toString().padStart(2, '0')} (hh:mm:ss)
+        </span>
       </div>
     </div>
   );
