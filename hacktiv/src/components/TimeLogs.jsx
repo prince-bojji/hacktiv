c489478cbd9ed9daee31413589c733019f2582ca
awoo1000 <maramagcheyenne1@gmail.com>
2023-09-13 12:25:13 +0800

    modified TimeLogs

--- hacktiv/src/components/TimeLogs.jsx
+++ hacktiv/src/components/TimeLogs.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import ProjectTracker from './ProjectTracker';
 
 function TimeLogs() {
     const monthNames = ["January", "February", "March", "April", "May", "June",
@@ -11,108 +10,115 @@ function TimeLogs() {
         {
             day: 'September 6, 2023',
             previous_time_in: '8:02',
-            previous_time_out: '17:00'
+            previous_time_out: '17:00',
+            work_hours: '08:58'
         },
         {
             day: 'September 7, 2023',
             previous_time_in: '8:00',
-            previous_time_out: '17:04'
+            previous_time_out: '17:04',
+            work_hours: '08:56'
         },
         {
             day: 'September 8, 2023',
             previous_time_in: '8:00',
-            previous_time_out: '17:00'
+            previous_time_out: '17:00',
+            work_hours: '09:00'
         },
         {
             day: 'September 11, 2023',
             previous_time_in: '8:04',
-            previous_time_out: '17:01'
+            previous_time_out: '17:01',
+            work_hours: '08:57'
         },
         {
             day: 'September 12, 2023',
             previous_time_in: '8:01',
-            previous_time_out: '17:00'
+            previous_time_out: '17:00',
+            work_hours: '08:59'
         },
         {
-            day: monthNames[date.getMonth()] + ' ' + date.getDate()+ ', ' + date.getFullYear(),
+            day: monthNames[date.getMonth()] + ' ' + date.getDate() + ', ' + date.getFullYear(),
             previous_time_in: '-',
-            previous_time_out: '-'
+            previous_time_out: '-',
+            work_hours: '-'
         },
     ];
 
-    const [projectTracker, setProjectTracker] = useState(false);
     const [timeIn, setTimeIn] = useState('-');
-    const [timedIn, setTimedIn] = useState(false);
     const [timeOut, setTimeOut] = useState('-');
-    const [timedOut, setTimedOut] = useState(false);
+    const [timerStart, setTimerStart] = useState(false);
 
-    const onProjectTracker = () => {
-        setProjectTracker(true);
+    // Add the functionality for the timer to start if timerStart is set to true
+    // and to stop if timerStart is set to false
+    // This task is meant for Cedrick 
+    const onTimerStart = () => {
+        const date = new Date();
+        let minutes = (date.getMinutes() < 10 ? '0' : '') + date.getMinutes();
+
+        { timeIn === '-' && setTimeIn(date.getHours() + ":" + minutes) }
+        setTimerStart(true);
     }
 
-    let minutes = (date.getMinutes()<10?'0':'') + date.getMinutes()
+    const onTimerStop = () => {
+        const date = new Date();
+        let minutes = (date.getMinutes() < 10 ? '0' : '') + date.getMinutes();
 
-    const onTimeIn = () => {
-        setTimeIn(date.getHours() + ":" + minutes);       
-        setTimedIn(true);
-    }
-
-    const onTimeOut = () => {
-        setTimeOut(date.getHours() + ":" + minutes);
-        setTimedOut(true);
+        { timeOut === '-' && setTimeOut(date.getHours() + ":" + minutes) }
+        setTimerStart(false);
     }
 
     return (
         <div>
-            {!projectTracker ? (
-                <>
-                    <div>
-                        <button
-                            className="bg-accent py-2 rounded text-secondary-a min-[320px]:px-1 sm:px-7 lg:text-lg xl:font-normal mb-4 float-right mr-5"
-                        >Attendance
-                        </button>
-                        <button
-                            className="bg-accent py-2 rounded text-secondary-a min-[320px]:px-1 sm:px-7 lg:text-lg xl:font-normal mb-4 float-right mr-5"
-                            onClick={onProjectTracker}>Project Tracker
-                        </button>
-                        <div className="w-full">
-                            <div className="mt-2 text-xl font-bold border border-secondary-a rounded-t p-4 flex bg-accent-a w-full h-[65px] items-center">
-                                Time Records
-                            </div>
-                            <div className={"bg-primary dark:bg-gray-800 border rounded-b p-1 xs:text-xs md:text-lg xl:text-lg min-[320px]:w-[100%] md:w-[100%] xl:w-[100%] h-max shadow-[0_0_2px_1px_rgba(0,0,0,0.25)] ease-in duration-100"}>
-                                <div className='flex font-bold justify-between items-center'>
-                                    <p className="pl-5">Date</p>
-                                    <button
-                                        className="bg-accent border border-secondary-a py-2 rounded text-secondary-a mt-2 mb-2 min-[320px]:ml-[45px] min-[320px]:p-1 min-[320px]:text-xs sm:text-lg sm:px-5 sm:ml-[70px] md:ml-[87px] lg:ml-[97px] xl:ml-[120px]"
-                                        onClick={onTimeIn} disabled={timedIn}
-                                    >Time in
-                                    </button>
-                                    <button
-                                        className="bg-accent border border-secondary-a py-2 rounded text-secondary-a mt-2 mb-2 min-[320px]:p-1 min-[320px]:mr-2 min-[320px]:text-xs sm:px-7 sm:text-lg sm:mr-[30px] lg:mr-[63px] xl:mr-[140px]"
-                                        onClick={onTimeOut} disabled={timedOut}
-                                    >Time out
-                                    </button>
-                                </div>
+            <p className="pl-2 text-xl font-bold float-right mt-0.5">00:00</p>
+            <div className="flex relative items-center float-right mb-5">
+                {!timerStart ? (
+                    <>
+                        <i className="fa-solid fa-stopwatch absolute ml-3"></i>
+                        <button onClick={onTimerStart} className="bg-accent-a font-semibold h-8 px-2 pr-3 pl-8 text-lg border border-secondary-a">
+                            START</button>
+                    </>
+                ) : (
+                    <>
+                        <i className="fa-solid fa-stopwatch absolute ml-3"></i>
+                        <button onClick={onTimerStop} className="bg-accent-a font-semibold h-8 px-2 pr-3 pl-8 text-lg border border-secondary-a">
+                            STOP</button>
+                    </>
+                )}
+            </div>
 
-                                {workDays.map((workDay, index) =>
-                                    <>
-                                        <div className='flex p-2 px-5 min-[320px]:gap-5 sm:gap-0' key={index}>
-                                            <p className='w-[45%]'>{workDay.day}</p>
-                                            {workDay.previous_time_in !== '-' ? (
-                                                <p className='w-[40%] '>{workDay.previous_time_in}</p>
-                                            ) : <p className='w-[40%] '>{timeIn}</p>}
-                                            {workDay.previous_time_out !== '-' ? (
-                                                <p>{workDay.previous_time_out}</p>
-                                            ) : <p>{timeOut}</p>}
-                                        </div>
-                                        {index < workDays.length - 1 && <hr />}
-                                    </>
-                                )}
-                            </div>
-                        </div>
+            <div>
+                <div className="w-full">
+                    <div className="mt-2 text-xl font-bold border border-secondary-a rounded-t p-4 flex bg-accent-a w-full h-[65px] items-center">
+                        Time Records
                     </div>
-                </>
-            ) : <ProjectTracker />}
+                    <div className={"bg-primary dark:bg-gray-800 border rounded-b p-1 xs:text-xs md:text-lg xl:text-lg min-[320px]:w-[100%] md:w-[100%] xl:w-[100%] h-max shadow-[0_0_2px_1px_rgba(0,0,0,0.25)] ease-in duration-100"}>
+                        <div className='flex font-bold justify-between items-center min-[320px]:text-sm sm:text-lg'>
+                            <p className="w-[25%] pl-5">Date</p>
+                            <p className="w-[25%] pl-[50px] ">Time in</p>
+                            <p className="w-[25%] min-[320px]:pl-[35px] sm:pl-[50px]">Time out</p>
+                            <p className="w-[25%] min-[320px]:pl-[20px] sm:pl-[50px]">Work hours</p>
+
+                        </div>
+
+                        {workDays.map((workDay, index) =>
+                            <>
+                                <div className='flex py-2 justify-between items-center min-[320px]:text-sm sm:text-lg' key={index}>
+                                    <p className='w-[25%] pl-5'>{workDay.day}</p>
+                                    {workDay.previous_time_in !== '-' ? (
+                                        <p className='w-[25%] pl-[50px]'>{workDay.previous_time_in}</p>
+                                    ) : <p className='w-[25%] pl-[50px]'>{timeIn}</p>}
+                                    {workDay.previous_time_out !== '-' ? (
+                                        <p className='w-[25%] min-[320px]:pl-[35px] sm:pl-[50px]'>{workDay.previous_time_out}</p>
+                                    ) : <p className='w-[25%] min-[320px]:pl-[35px] sm:pl-[50px]'>{timeOut}</p>}
+                                    <p className='w-[25%] min-[320px]:pl-[20px] sm:pl-[50px]'>{workDay.work_hours}</p>
+                                </div>
+                                {index < workDays.length - 1 && <hr />}
+                            </>
+                        )}
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
