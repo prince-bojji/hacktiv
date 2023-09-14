6ec0fbe0ae775625a8a785d0c90d0e360055def0
jeyhan22 <74844330+jeyhan22@users.noreply.github.com>
2023-09-14 10:01:19 +0800

    Saving the test configuration

--- hacktiv/src/pages/Test.jsx
+++ hacktiv/src/pages/Test.jsx
@@ -1,23 +1,65 @@
-import React, { Component } from 'react';
-import jsonData from '../data/db.json'; // Import the JSON data
+import React, { useState } from 'react';
+import axios from 'axios';
 
-class Test extends Component {
-  render() {
-    return (
-      <div>
-        <h1>User Data</h1>
-        <ul>
-          {jsonData.users.map((user) => (
-            <li key={user.id}>
-              <strong>Name:</strong> {user.name}<br />
-              <strong>Email:</strong> {user.email}<br />
-              <strong>Password:</strong> {user.password}
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
-  }
+function Test() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const newUser = {
+      name,
+      email,
+      password,
+    };
+
+    try {
+      // Send a POST request to the server at http://localhost:3000/users
+      await axios.post('http://localhost:3000/users', newUser);
+
+      // Clear the input fields
+      setName('');
+      setEmail('');
+      setPassword('');
+    } catch (error) {
+      console.error('Error inserting data:', error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Add User</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Name:</label>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Email:</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit">Add User</button>
+      </form>
+    </div>
+  );
 }
 
 export default Test;
